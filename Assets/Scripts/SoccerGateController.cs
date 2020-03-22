5e0ec2486142f3f2dd7e4576c736eb16d4d53bf5
Godzzo <egodzzo@gmail.com>
2020-03-22 10:08:31 +0100

    Red and Blue Soccer Gates added and check for Goalll!!! 🤩 🤩

---
+++ Assets/Scripts/SoccerGateController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SoccerGateController : MonoBehaviour
+{
+    public GoalEvent goalEvent;
+    public int gatePos = 0;
+
+    void Start() { }
+    void Update() { }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball") {
+            goalEvent.Invoke(gatePos);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+    }
+}
+
+[System.Serializable]
+public class GoalEvent : UnityEvent<int> { }
--- Assets/Scripts/SoccerPlayerController.cs
+++ Assets/Scripts/SoccerPlayerController.cs
@@ -5,27 +5,41 @@ using UnityEngine.AI;
 
 public class SoccerPlayerController : MonoBehaviour
 {
-    public GameObject ball;
     public int kickPower = 100;
     public float ballDistance = 1.5f;
 
+    GameObject[] _balls;
     PlayerController _playerController;
 
     void Start()
     {
         _playerController = GetComponent<PlayerController>();
+
+        _balls = GameObject.FindGameObjectsWithTag("Ball");
     }
 
     // Update is called once per frame
     void Update()
     {
+        foreach (var ball in _balls)
+        {
+            KickBalls(ball);
+        }
+    }
+
+    private void KickBalls(GameObject ball)
+    {
         if (Vector3.Distance(_playerController.player.transform.position, ball.transform.position) < ballDistance)
         {
             var agent = _playerController.player.GetComponent<NavMeshAgent>();
-            
+
             // _playerController.player.transform.forward
             ball.GetComponent<Rigidbody>().AddForce(agent.velocity.normalized * kickPower * agent.velocity.magnitude, ForceMode.Impulse);
         }
     }
 
+    public void OnGoal(int gatePos)
+    {
+        Debug.Log("GOALLLL!!!!! " + gatePos);
+    }
 }
